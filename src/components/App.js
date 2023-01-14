866a4781986b3b928e60e294ec65c9d75c650a92
lizdmukami <lizdmukami@gmail.com>
2023-01-14 14:42:32 +0300

    ass down the task data from App to TaskList, and display each task using the Task component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,23 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [task, setTask] = useState(TASKS)
+  // const [categories, setCategories] = useState(CATEGORIES)
+  // const [selectedCategory, setSelectedCategory] = useState("All")
+
+
+  function addNewTask(newItem) {
+    setTask([...task, newItem])
+  }
+
+  const itemDisplayed = task
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={itemDisplayed}/>
     </div>
   );
 }
